dc8f16d50b378b6a5578fcfc8be26e80822e9710
Jesse Sheehan <jesse@sheehan.nz>
2019-01-14 20:29:01 +1300

    Deleted the test json data. Obfuscated a url.

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 /**
  * The root of all requests.
  */
-export const BASE = "https://www.stuff.co.nz/";
+export const BASE = "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x74\x75\x66\x66\x2E\x63\x6F\x2E\x6E\x7A\x2F";
 
 /**
  * The path to the JSON API.
