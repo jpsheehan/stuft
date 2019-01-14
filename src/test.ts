f528f23dfb726469c77a9eea4527998cc8511cf0
Jesse Sheehan <jesse@sheehan.nz>
2019-01-14 21:17:06 +1300

    Removed test file.

--- src/test.ts
+++
@@ -1,7 +0,0 @@
-import stuft from "./index";
-
-(async () => {
-  const articles = await stuft({ id: 109911861 });
-  // tslint:disable-next-line
-  articles.forEach((article) => console.log(`#${article.id}: ${article.title}`));
-})();
