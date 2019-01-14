6a3c228d0cc3e572d2b2bee7d7bbfe828ffe0f35
Jesse Sheehan <jesse@sheehan.nz>
2019-01-14 21:56:24 +1300

    Added cross-fetch as a dependency.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import https from "https";
+import fetch from "cross-fetch";
 
 import Article, { IArticleArguments } from "./Article";
 import {
@@ -31,59 +31,55 @@ function buildUrl(options: IStuftOptions): URL {
 }
 
 /**
- * Parses the raw HTTP response and returns a list of articles.
- * @param rawData The raw response.
+ * Parses the json response and returns a list of articles.
+ * @param json The raw response.
  */
-function parseResponse(rawData: string): Article[] {
-  const parsedData = JSON.parse(rawData);
-  const { stories } = parsedData;
-
-  if (stories) {
-    return stories.map((json: IArticleArguments) => new Article(json));
-  } else {
-    return [ new Article(parsedData) ];
-  }
+function parseAggregateResponse(json: IAggregateResponse): Article[] {
+  return json.stories.map((a) => new Article(a));
 }
 
 /**
- * Sends a request to retrieve a number of news articles.
+ * Parses the json response and returns an article.
+ * @param json The raw response.
+ */
+function parseSingletonResponse(json: IArticleArguments): Article {
+  return new Article(json);
+}
+
+/**
+ * Sends a request to retrieve a number of news articles. By default, returns the first 5 articles.
+ * The `options` are as follows:
+ * - Set `limit` to retrieve a specific number of articles.
+ * - Set `section` to retrieve articles from a specific section.
+ * - Set `id` to retrieve a single article.
+ * - Set `fetchOptions` to pass additional parameters to fetch.
  * @param options The options used to request articles.
  */
-export default function stuft(options: IStuftOptions = {}): Promise<Article[]> {
-  return new Promise((resolve, reject) => {
-    try {
-      const url = buildUrl(options);
+export default async function stuft(options: IStuftOptions = {}): Promise<Article[]> {
+  const { id, fetchOptions } = options;
+  const url = buildUrl(options);
+  const res = await fetch(url.toString(), fetchOptions);
 
-      https.get(url, (res) => {
-        const contentType = res.headers["content-type"] || "";
-        const { statusCode } = res;
+  // check for errors
+  if (res.status !== 200) {
+    throw new Error(`Request Failed. Status Code: ${res.status}`);
+  }
 
-        // check for errors
-        let error;
-        if (statusCode !== 200) {
-          error = new Error(`Request Failed. Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-          error = new Error(`Invalid content-type. Expected 'application/json' but received ${contentType}`);
-        }
-        if (error) {
-          res.resume();
-          reject(error);
-        }
-
-        // download the data
-        let rawData = "";
-        res.on("data", (chunk) => { rawData += chunk; });
-        res.on("error", (err) => reject(err));
-        res.on("end", () => resolve(parseResponse(rawData)));
-      });
-    } catch (err) {
-      reject(err);
-    }
-  });
+  const json = await res.json();
+  if (id) {
+    return [parseSingletonResponse(json)];
+  } else {
+    return parseAggregateResponse(json);
+  }
 }
 
 interface IStuftOptions {
   id?: number;
   limit?: number;
   section?: Section | string;
+  fetchOptions?: any;
+}
+
+interface IAggregateResponse {
+  stories: IArticleArguments[];
 }
